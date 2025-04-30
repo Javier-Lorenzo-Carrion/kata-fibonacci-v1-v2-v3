f62f3534fb22b2dc6c375ed4f06c5aeae183571a
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-04-30 12:40:46 +0100

    test: add new test case for number 6 already cover by production code

--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -19,4 +19,7 @@ describe('Fibonacci version 1 should', () => {
   it('return 5 for case 5 ', () => {
     expect(fibonacciV1(5)).toBe(5);
   });
+  it('return 8 for case 6 ', () => {
+    expect(fibonacciV1(6)).toBe(8);
+  });
 });
